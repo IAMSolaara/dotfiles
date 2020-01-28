38744643233deecfa6512e4de8064767cff2212c
lorecast162 <lorecastcauli@gmail.com>
2020-01-28 19:09:05 +0000

    tried to merge most thigns

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -7,13 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 #have default network interface(s) available as variable
-<<<<<<< HEAD
-export DEFAULT_ETHERNET=$(ip route | grep '^default' | awk '{print $5}' | grep e)
-export DEFAULT_WIFI=$(ip route | grep '^default' | awk '{print $5}' | grep w)
-=======
 export DEFAULT_ETHERNET=$(ip route | grep '^default' | awk '{print $5}' | grep e | uniq)
 export DEFAULT_WIFI=$(ip route | grep '^default' | awk '{print $5}' | grep w | uniq)
->>>>>>> 0473360bdbf8ab17f67f1be83fd60673937cef40
 
 #launch polybar with default config
 #polybar vgabar1 &
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 export EDITOR=vim
-=======
->>>>>>> 0473360bdbf8ab17f67f1be83fd60673937cef40
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH=$HOME/.local/bin:/usr/local/bin:$HOME/n64sdk/tools/bin:/opt/gbdk/bin:/opt/gbxcart:$HOME/dotfiles/scripts:$PATH
 if systemctl -q is-active graphical.target && [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
