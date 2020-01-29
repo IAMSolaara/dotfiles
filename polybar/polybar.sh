0c4dedeac91d3390eecc8f1ee7d52d9b0c05eeae
lorecast162 <lorecastcauli@gmail.com>
2020-01-29 18:24:24 +0100

    added some nice things, including twmn for notifications, a notification sound for when I need it and the OSX Leopart Aurora wallpaper

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -7,8 +7,11 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 #have default network interface(s) available as variable
-export DEFAULT_ETHERNET=$(ip route | grep '^default' | awk '{print $5}' | grep e | uniq)
-export DEFAULT_WIFI=$(ip route | grep '^default' | awk '{print $5}' | grep w | uniq)
+#export DEFAULT_ETHERNET=$(ip route | grep '^default' | awk '{print $5}' | grep e | uniq)
+#export DEFAULT_WIFI=$(ip route | grep '^default' | awk '{print $5}' | grep w | uniq)
+
+export DEFAULT_ETHERNET="ens5"
+export DEFAULT_WIFI="wlan0"
 
 #launch polybar with default config
 #polybar vgabar1 &
